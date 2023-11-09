9a92c3222a09c959d2d0848d6242890da8cab906
peemknn <kunanontpeem333@gmail.com>
2023-11-09 20:07:37 +0700

    Implemented nextauth and test login

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
+import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
-<main>
-  <h1 className="text-2xl text-center font-bold"> Hi </h1>
-</main>
-  )
+    <main>
+      <h1 className="text-2xl text-center font-bold"> Hi </h1>
+      <Link href="api/auth/signin">SignIn</Link>
+    </main>
+  );
 }
