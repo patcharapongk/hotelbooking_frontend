1dd6e34293c48e40a5852a0c0e306744d582c90a
peemknn <kunanontpeem333@gmail.com>
2023-11-09 21:05:00 +0700

    Change userLibs import path

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth/next";
 import { AuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
-import userLogin from "@/libs/userLogin";
+import userLogin from "@/lib/userLogin";
 
 export const authOptions: AuthOptions = {
   providers: [
